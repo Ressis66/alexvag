6af084ddc298f724950e3a17095ab3ada267903f
Ressis66 <vaganov.aleksandr.88@mail.ru>
2022-09-16 12:30:23 +0500

    Initial commit

---
+++ src/main/java/ru/batchproject/alexvag/AlexvagApplication.java
@@ -0,0 +1,13 @@
+package ru.batchproject.alexvag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexvagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexvagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/batchproject/alexvag/AlexvagApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.batchproject.alexvag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexvagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
